361d3ce5ba93f07661ec7ba19ed8e3b16fd3f499
Natalia-mswm <149807177+Natalia-mswm@users.noreply.github.com>
2024-04-08 22:27:42 +0500

    Added file
    Добавлен файл (переводчик, стримлит)

---
+++ Maksimova_translator.py
@@ -0,0 +1,13 @@
+# Группа 1, Максимова Н.В.
+from transformers import pipeline
+import streamlit as st
+
+
+st.title("Перевод текста с русского на французский")
+translator = pipeline("translation_ru_to_fr", "Helsinki-NLP/opus-mt-ru-fr")
+text = st.text_input('Введите текст для перевода:', 'Введите текст')
+button = st.button('Перевести')
+if button:
+    #    a = translator(text)
+    #   translated_text = translate()
+    text2 = st.write("Перевод: ", translator(text))
